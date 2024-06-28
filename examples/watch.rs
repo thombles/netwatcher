d080463c3c86ac4729f5e0d99332b8f556811755
Thomas Karpiniec <tom.karpiniec@outlook.com>
2024-06-28 11:16:09 +1000

    Make example program test dropping the WatchHandle

--- examples/watch.rs
+++ examples/watch.rs
@@ -1,13 +1,18 @@
 use std::time::Duration;
 
 fn main() {
-    let _handle = netwatcher::watch_interfaces(|update| {
+    println!("Watching for changes for 30 seconds...");
+
+    let handle = netwatcher::watch_interfaces(|update| {
         println!("Interface update!");
         println!("State: {:?}", update.interfaces);
         println!("Diff: {:?}", update.diff);
     });
 
-    loop {
-        std::thread::sleep(Duration::from_secs(60));
-    }
+    std::thread::sleep(Duration::from_secs(30));
+
+    drop(handle);
+    println!("Stopped watching! Program will end in 30 seconds.");
+
+    std::thread::sleep(Duration::from_secs(30));
 }
